ca5ac28a241989d37c8e8daff628d0ba492f79e4
jian long <jianlong@jiandeMacBook-Air.local>
2019-01-19 22:42:00 +0800

    init

---
+++ src/main/java/taoquangong/cn/couponpal/CouponpalApplication.java
@@ -0,0 +1,14 @@
+package taoquangong.cn.couponpal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponpalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouponpalApplication.class, args);
+    }
+
+}
+
